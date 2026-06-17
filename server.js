7d3e700ebd936f4388d99e8e9fa6cc5697ed2c97
Aayushi Singh <aayushisinghrajput87@gmail.com>
2026-06-17 11:18:17 +0530

    function added

---
+++ server.js
@@ -0,0 +1,11 @@
+const a=8;
+const b=5;
+
+
+function add(a,b){
+    return a+b;
+}
+
+const res=()=>{
+    console.log("The Aayushi");
+}
---
+++ util.js
@@ -0,0 +1,6 @@
+const a=67;
+const b=45;
+
+function add(x,y){
+    return x+y;
+}
